06d8e0ac9142b2299323c82f7b340ae4300b7a45
harinath <harinath@careerscale.in>
2012-12-30 07:00:18 +0000

    Initial import

---
+++ ocms/src/main/java/in/careerscale/apps/ocms/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package in.careerscale.apps.ocms.config;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ImportResource(value = "classpath:spring-security-context.xml")
+public class SecurityConfig {}
---
+++ ocms/src/main/java/in/careerscale/apps/ocms/web/DefaultController.java
@@ -0,0 +1,11 @@
+package in.careerscale.apps.ocms.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+	@RequestMapping("/default/**/*")
+	public void defaultRequest() {}
+}
