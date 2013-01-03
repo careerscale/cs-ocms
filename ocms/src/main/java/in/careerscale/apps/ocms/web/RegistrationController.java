5c6e6fdf36931d76b8cbed2066f3130091db509c
harinath <harinath@careerscale.in>
2013-01-03 15:39:03 +0000

    registration form code -> need to be fixed

--- ocms/src/main/java/in/careerscale/apps/ocms/web/RegistrationController.java
+++ ocms/src/main/java/in/careerscale/apps/ocms/web/RegistrationController.java
@@ -1,5 +1,7 @@
 package in.careerscale.apps.ocms.web;
 
+import java.util.Date;
+
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,67 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Secured("ROLE_ANONYMOUS")
 public class RegistrationController {
 	
+//	private Integer id;
+	private String emailId;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirth;
+	
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String index(Model model) {
 		return "register/register";
 	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(Model model) {
+		
+		System.out.println(emailId);
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(dateOfBirth);
+		//
+		return "register/register"; //we need to return next page.
+	}
+	
 }
