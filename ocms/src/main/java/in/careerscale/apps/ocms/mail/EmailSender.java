a366e7adbc8c32954b8c65144d390182189f3799
harinath <harinath@careerscale.in>
2013-03-06 00:52:56 +0000

    Email code as is. prefer to use spring mail instead. will push changes when ready.

---
+++ ocms/src/main/java/in/careerscale/apps/ocms/mail/EmailSender.java
@@ -0,0 +1,141 @@
+package in.careerscale.apps.ocms.mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+
+
+/**
+ * refer for gmail authentication details.
+ * http://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
+ * 
+ * @author harinath
+ * 
+ */
+
+@Service("emailService")
+public class EmailSender {
+
+	private static Log logger = LogFactory.getLog(EmailSender.class);
+	private static final String host = "smtp.gmail.com";
+	private static final int SSLport = 465;
+	private static final int smtpPort = 587;
+	private static final String username = "webdemo2012@gmail.com";
+	private static final String password = "webdemo2012@123";
+	private static final String from = "webdemo2012@gmail.com";
+
+	/**
+	 * Send mail with TLS enabled mode
+	 * 
+	 * @param template
+	 * @param email
+	 */
+	public void sendEmail(String template, String email) {
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.port", smtpPort);
+		Session session = Session.getInstance(props,new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username,password);
+			}
+		});
+ 
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(email));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+					+ "\n\n No spam to my email, please!");
+
+			Transport transport = session.getTransport("smtp");
+     		transport.connect(host, smtpPort, username, password);
+
+			Transport.send(message);
+
+			logger.info("DONE sending email with TLS"); 
+			
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	/**
+	 * To send email with GMAIL account using SSL.
+	 * @param template the template name to be used.
+	 * @param email the email id to which the mail need to be sent.
+	 */
+	public static void sendMailWithSSL(String subject, String body, String email) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", SSLport);
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(from, password);
+					}
+				});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(email));
+			message.setSubject(subject);
+			message.setText(body);
+
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host,SSLport,username, password);
+			
+			Transport.send(message);
+			logger.info("DONE sending email with SSL"); 
+			
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Main method, describes how to invoke the email sending methods.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EmailSender sender = new EmailSender();
+		//commented next lines to skip the erring code.
+		//TODO when bandwidth is available fix it.
+	/*	try {
+			sender.sendEmail("dummy", "webdemo2012@gmail.com");
+		} catch (Exception e) {
+			logger.error("Error while sending email with TLS", e);
+			
+		}
+	*/	try {
+			sender.sendMailWithSSL("subject ","Message body", "webdemo2012@gmail.com");
+		} catch (Exception e) {
+			logger.error("Error while sending email with TLS", e);
+		}  
+	}
+}
--- ocms/src/main/java/in/careerscale/apps/ocms/service/UserService.java
+++ ocms/src/main/java/in/careerscale/apps/ocms/service/UserService.java
@@ -3,6 +3,8 @@ package in.careerscale.apps.ocms.service;
 import in.careerscale.apps.ocms.dao.UserRepository;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.PersistenceException;
@@ -17,53 +19,80 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import in.careerscale.apps.ocms.dao.model.LoginMaster;
+import in.careerscale.apps.ocms.mail.EmailSender;
+import in.careerscale.apps.ocms.mail.EmailTemplates;
+import in.careerscale.apps.ocms.mail.Template;
 import in.careerscale.apps.ocms.service.exception.ApplicationException;
 import in.careerscale.apps.ocms.web.registration.model.User;
 
-
-
-
-
 @Service("userService")
 public class UserService implements UserDetailsService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	@PostConstruct	
+
+	@Autowired
+	private EmailSender emailService;
+
+	@PostConstruct
 	protected void initialize() {
-		//userRepository.save(new User("user", "demo", "ROLE_USER"));
-		//userRepository.save(new User("admin", "admin", "ROLE_ADMIN"));
+		// userRepository.save(new User("user", "demo", "ROLE_USER"));
+		// userRepository.save(new User("admin", "admin", "ROLE_ADMIN"));
 	}
-	
+
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
 		LoginMaster user = userRepository.findByUsername(username);
-		if(user == null) {
+		if (user == null) {
 			throw new UsernameNotFoundException("user not found");
 		}
-		GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER"); //fix this with proper rle
-		return new org.springframework.security.core.userdetails.User(user.getEmailId(), user.getPassword(), Collections.singleton(authority));
+		GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER"); // fix
+																				// this
+																				// with
+																				// proper
+																				// rle
+		return new org.springframework.security.core.userdetails.User(
+				user.getEmailId(), user.getPassword(),
+				Collections.singleton(authority));
 	}
-	
-	public void registerUser(User user) throws ApplicationException{
-		//for bravity no validations done at service layer, we need to handle and also introduce valid exception handling to manage error situations
-		try{
-		userRepository.registerUser(new LoginMaster(user.getEmailId(),user.getPassword(),user.getFirstName(),user.getLastName(),user.getDateOfBirth()));
-		}catch(PersistenceException pe){
+
+	public void registerUser(User user) throws ApplicationException {
+		// for bravity no validations done at service layer, we need to handle
+		// and also introduce valid exception handling to manage error
+		// situations
+		try {
+			userRepository.registerUser(new LoginMaster(user.getEmailId(), user
+					.getPassword(), user.getFirstName(), user.getLastName(),
+					user.getDateOfBirth()));
+			try{
+			// Resolve variables
+			Map<String, String> placeHolderValues = new HashMap<String, String>();
+			placeHolderValues.put(EmailTemplates.firstName, user.getFirstName());
+			placeHolderValues.put(EmailTemplates.userName, user.getLastName());
+			placeHolderValues.put(EmailTemplates.password, user.getPassword());
+			String emailText = EmailTemplates.getEmailMessage(
+					Template.Registration, placeHolderValues);
+			emailService.sendEmail(emailText, user.getEmailId());
+			}catch(Exception mailFailure){
+				mailFailure.printStackTrace();
+				//TODO this is just stupid to print stacktrace. log it buddy.
+			}
+		} catch (PersistenceException pe) {
 			throw new ApplicationException(pe.getMessage());
 		}
-	
+
 	}
 
 	public User findByUsername(String username) {
-	
+
 		LoginMaster loginMaster = userRepository.findByUsername(username);
-		
-		if(loginMaster != null){
-			return new User(loginMaster.getEmailId(), loginMaster.getFirstName(), loginMaster.getLastName(),loginMaster.getDateOfBirth());
-		}
-		else{
+
+		if (loginMaster != null) {
+			return new User(loginMaster.getEmailId(),
+					loginMaster.getFirstName(), loginMaster.getLastName(),
+					loginMaster.getDateOfBirth());
+		} else {
 			return null;
 		}
 	}
