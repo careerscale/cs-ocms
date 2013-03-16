683ed580db82bf64fad3a460cf5ec4193e2109bb
harinath <harinath@careerscale.in>
2013-03-16 03:42:50 +0000

    property resolver issue resolved.
    If you are adding a new property file, remember to add it in RootConfig.java.
    
    As we go let us try to see which one is really the way to go. 
    
    XML changes are not honoured or overridden by the changes in RootConfig

--- ocms/src/main/java/in/careerscale/apps/ocms/config/RootConfig.java
+++ ocms/src/main/java/in/careerscale/apps/ocms/config/RootConfig.java
@@ -3,6 +3,7 @@ package in.careerscale.apps.ocms.config;
 import org.springframework.context.annotation.*;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @ComponentScan(basePackages = { "in.careerscale.apps.ocms" })
@@ -11,7 +12,12 @@ public class RootConfig {
 	@Bean
 	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
 		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		//ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		
+		 Resource[] resources = new ClassPathResource[ ]
+				   { new ClassPathResource( "persistence.properties" ), new ClassPathResource( "application.properties" ) };
+		  ppc.setLocations( resources );
+				  ppc.setIgnoreUnresolvablePlaceholders( true );
 		return ppc;
 	}
 	
