7ec04a5ae93bd303e4900d42b0ff4badb57e5f13
harinath@careerscale.in <harinath@careerscale.in@6da47581-2468-b133-9240-cb7a334c863c>
2013-04-03 15:57:59 +0000

    minor changes and case controller added.
    Bugs are still present. :(.

---
+++ trunk/ocms/src/main/java/in/careerscale/apps/ocms/web/cases/CaseController.java
@@ -0,0 +1,34 @@
+package in.careerscale.apps.ocms.web.cases;
+
+
+
+import in.careerscale.apps.ocms.service.UserService;
+import in.careerscale.apps.ocms.web.registration.model.User;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Secured("ROLE_USER")
+public class CaseController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CaseController.class);
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "cases", method = RequestMethod.GET)	
+	public String index(UserDetails userDetails, Model model) {
+		LOG.info(userDetails.toString());
+		return "cases/index";
+	}
+}
---
+++ trunk/ocms/src/main/webapp/WEB-INF/views/cases/index.jsp
@@ -0,0 +1,29 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
+<%@ taglib prefix="security"
+	uri="http://www.springframework.org/security/tags"%>
+
+
+
+
+
+<div id="page-container" class="resize">
+
+	<div id="page-content-inner" class="resize">
+		<h3>
+			Hello
+			<security:authentication property="principal.username" />
+			!
+		</h3>
+		
+		<h1>Work is in progress... Please visit us again</h1>
+		
+		<p>
+			Click <a href='<s:url value="/"></s:url>'>here</a> to navigate to
+			home page. Click <a href='<s:url value="/logout"></s:url>'>here</a>
+			to logout.
+		</p>
+
+	</div>
+</div>
+
