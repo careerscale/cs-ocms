37c06d4895c84a6cb7b6c377afc3825612710d7c
harinath@careerscale.in <harinath@careerscale.in@6da47581-2468-b133-9240-cb7a334c863c>
2013-04-05 03:32:36 +0000

    email id temp fix for linkedin. instead of email id, adding linked id itself on user object

--- trunk/ocms/src/main/java/in/careerscale/apps/ocms/web/oauth/AuthrnicationController.java
+++ trunk/ocms/src/main/java/in/careerscale/apps/ocms/web/oauth/AuthrnicationController.java
@@ -2,6 +2,8 @@ package in.careerscale.apps.ocms.web.oauth;
 
 import in.careerscale.apps.ocms.web.registration.model.User;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,10 +12,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuthrnicationController
 {
+	Log log = LogFactory.getLog(AuthrnicationController.class);
 	private static AuthenticationManager am = new SampleAuthenticationManager();
 	
 	public boolean authenticate(String emailAddress){
-		System.out.println("Inside authenticate:");
+		log.info("Inside authenticate:");
 		
 		 try {
 		        Authentication request = new UsernamePasswordAuthenticationToken(emailAddress, "password");
@@ -26,7 +29,7 @@ public class AuthrnicationController
 		    	  System.out.println("Authentication failed general Exception: " + e.getMessage());
 			}
 		 
-		 System.out.println("Successfully authenticated. Security context contains: " +
+		 log.info("Successfully authenticated. Security context contains: " +
 	              SecurityContextHolder.getContext().getAuthentication());
 		return false;
 	}
@@ -34,7 +37,7 @@ public class AuthrnicationController
 	public boolean authenticate(User user)
 	{
 		
-		System.out.println("Inside authenticate:");
+		log.info("Inside authenticate:");
 		
 		 try {
 		        //Authentication request = new UsernamePasswordAuthenticationToken(user.getEmailId(), "");
@@ -50,7 +53,7 @@ public class AuthrnicationController
 		    	  System.out.println("Authentication failed general Exception: " + e.getMessage());
 			}
 		 
-		 System.out.println("Successfully authenticated. Security context contains: " +
+		 log.info("Successfully authenticated. Security context contains: " +
 	              SecurityContextHolder.getContext().getAuthentication());
 		return false;
 	}
