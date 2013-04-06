83b60ccb8bd38a7d1fbe789b6600347a944d4c5b
harinath@careerscale.in <harinath@careerscale.in@6da47581-2468-b133-9240-cb7a334c863c>
2013-04-06 18:39:09 +0000

    add case

--- trunk/ocms/src/main/java/in/careerscale/apps/ocms/web/cases/CaseController.java
+++ trunk/ocms/src/main/java/in/careerscale/apps/ocms/web/cases/CaseController.java
@@ -2,39 +2,95 @@ package in.careerscale.apps.ocms.web.cases;
 
 
 
-import in.careerscale.apps.ocms.service.UserService;
-import in.careerscale.apps.ocms.web.registration.model.User;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import in.careerscale.apps.ocms.service.CaseService;
+import in.careerscale.apps.ocms.service.MasterDataService;
 
+import in.careerscale.apps.ocms.service.exception.ApplicationException;
+import in.careerscale.apps.ocms.web.cases.model.Case;
+import in.careerscale.apps.ocms.web.registration.RegistrationController;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Secured("ROLE_USER")
-public class CaseController {
+public class CaseController implements Validator  {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(CaseController.class);
+	Log log = LogFactory.getLog(CaseController.class);
 	
 	@Autowired
-	private UserService userService;
+	private CaseService caseService;
 	
-	@RequestMapping(value = "cases", method = RequestMethod.GET)	
-	public String index(UserDetails userDetails, Model model) {
-		LOG.info(userDetails.toString());
-		return "cases/index";
+	@Autowired
+	private MasterDataService masterDataService;
+	/*@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(HttpServletRequest request,
+			HttpServletResponse response) {
+		log.debug("within login GET method call");
+
+		return "register/login";
+	}*/
+	
+	@RequestMapping(value = "/case", method = RequestMethod.GET)	
+	public String index(@ModelAttribute(value = "case") Case bean,
+			BindingResult errors, HttpServletRequest request,
+			HttpServletResponse response) throws Exception
+			{
+				setMasterData(bean);
+				return "cases/addcases";
+		
 	}
 	
-	@RequestMapping(value = "cases/active", method = RequestMethod.GET)	
-	public String activeCaess(UserDetails userDetails, Model model) {
-		LOG.info(userDetails.toString());
-		return "cases/activecases";
+	@RequestMapping(value = "/case", method = RequestMethod.POST)
+	public String register(@ModelAttribute(value = "case") Case bean,
+			BindingResult errors, HttpServletRequest request,
+			HttpServletResponse response) throws Exception
+			{
+		try{
+			caseService.registerCase(bean);
+		}catch(ApplicationException ae){
+			errors.addError(new ObjectError("view.case.registration.error", "case is already registered, please choose another one" ));
+        	setMasterData(bean);
+			return "cases/addcases";
+			
+		}
+		return "cases/registeredcases";
+			}
+	
+	
+	private void setMasterData(Case bean){
+		try {
+			bean.setCaseMasterTypes(masterDataService.getCaseTypes1());
+			bean.setHelpMasterTypes(masterDataService.getHelpTypes());
+		} catch (ApplicationException e) {
+			log.error("error while retrieving master data", e);
+		}
+	}
+	
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		
 	}
 }
