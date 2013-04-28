dbafd21e93d60be7290286fe38254882de4e237e
harinath@careerscale.in <harinath@careerscale.in@6da47581-2468-b133-9240-cb7a334c863c>
2013-04-28 09:55:20 +0000

    need to fix the mocking issue with powermock.

---
+++ trunk/ocms/src/test/java/in/careerscale/apps/ocms/service/CaseServiceTest.java
@@ -0,0 +1,59 @@
+package in.careerscale.apps.ocms.service;
+
+import in.careerscale.apps.ocms.config.PersistenceConfig;
+import in.careerscale.apps.ocms.config.RootConfig;
+import in.careerscale.apps.ocms.service.exception.ApplicationException;
+import in.careerscale.apps.ocms.web.cases.model.Case;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes=RootConfig.class,loader=AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes={ RootConfig.class, PersistenceConfig.class})
+@WebAppConfiguration
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public class CaseServiceTest {
+	
+	@Mock
+	SecurityContext mockSecurityContext;   
+	
+
+	@Autowired
+	private CaseService caseService;
+	
+	@Test
+	public void registerCase() throws ApplicationException{
+                
+		ExtendedUser extendedUser = PowerMockito.mock(ExtendedUser.class);
+		Authentication auth = PowerMockito.mock(Authentication.class);
+		PowerMockito.mockStatic(SecurityContextHolder.class);
+		PowerMockito.when(SecurityContextHolder.getContext()).thenReturn(mockSecurityContext);
+		PowerMockito.when(mockSecurityContext.getAuthentication()).thenReturn(auth);
+		PowerMockito.when(auth.getPrincipal()).thenReturn(extendedUser);
+		
+		Case caseBean = new Case("dummycase", "dummy description", "new paper", Calendar.getInstance().getTime(), Calendar.getInstance().getTime(), "232323");
+		caseService.registerCase(caseBean);
+	
+	}
+	
+	
+	
+
+}
