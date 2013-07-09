987dfd0fe9f789e46e7d8355c479006f56acdaf5
harinath@careerscale.in <harinath@careerscale.in@6da47581-2468-b133-9240-cb7a334c863c>
2013-07-09 10:36:45 +0000

    dao and service changes.
    in progress.

--- trunk/ocms/src/main/java/in/careerscale/apps/ocms/dao/model/CaseStatus.java
+++ trunk/ocms/src/main/java/in/careerscale/apps/ocms/dao/model/CaseStatus.java
@@ -1,12 +1,27 @@
 package in.careerscale.apps.ocms.dao.model;
 
+import in.careerscale.apps.ocms.util.CommonUtil;
 
-	public enum CaseStatus {
-		New(0),Pending(1),InProgess(2),OnHold(3),Resolved(4);
-		
-		private CaseStatus(int id) {
-		}
+public enum CaseStatus
+{
 
-	
+	New(1), Pending(2), OnHold(3), Active(4), Rejected(5), Resolved(6), Closed(7);
+
+	private CaseStatus(int id)
+	{
+		this.id = id;
+	}
+
+	public static CaseStatus fromString(String name)
+	{
+		return CommonUtil.getEnumFromString(CaseStatus.class, name);
+	}
+
+	public int getId()
+	{
+		return this.id;
+	}
+
+	private int id;
 
 }
