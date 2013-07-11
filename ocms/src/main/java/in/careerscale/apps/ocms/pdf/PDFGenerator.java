6b5edabe5b2de1b61d93a5d83b42b1fecad65d64
harinath <harinath@careerscale.in>
2013-07-11 16:24:55 +0000

    TMAD logo and digital certificates

--- ocms/src/main/java/in/careerscale/apps/ocms/pdf/PDFGenerator.java
+++ ocms/src/main/java/in/careerscale/apps/ocms/pdf/PDFGenerator.java
@@ -3,6 +3,7 @@ package in.careerscale.apps.ocms.pdf;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.PrivateKey;
@@ -13,14 +14,23 @@ import java.util.Date;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import com.ibm.icu.util.Calendar;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfSignatureAppearance;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 import com.itextpdf.text.pdf.security.BouncyCastleDigest;
 import com.itextpdf.text.pdf.security.CrlClient;
 import com.itextpdf.text.pdf.security.DigestAlgorithms;
@@ -34,6 +44,12 @@ import com.itextpdf.text.pdf.security.TSAClient;
 
 public class PDFGenerator
 {
+
+	private Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 15, Font.BOLD);
+	private Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.RED);
+	private Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+	private Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 8, Font.BOLD);
+	private Font plainFont = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.BOLD);
 
 	public static void sign(String src, String name, String dest, Certificate[] chain, PrivateKey pk,
 			String digestAlgorithm, String provider, CryptoStandard subfilter, String reason, String location)
@@ -54,18 +70,126 @@ public class PDFGenerator
 		MakeSignature.signDetached(appearance, digest, pks, chain, null, null, null, 0, subfilter);
 	}
 
-	public static final String SRC = "hello.pdf";
-	public static final String DEST = "hello_cacert.pdf";
-
-	public static void main(String[] args) throws IOException, GeneralSecurityException, DocumentException
+	private void addPageTitle(Document document) throws DocumentException, MalformedURLException, IOException
 	{
-		/**
-		 * Properties properties = new Properties(); properties.load(new
-		 * FileInputStream("c:/home/blowagie/key.properties")); String path = properties.getProperty("PRIVATE"); char[]
-		 * pass = properties.getProperty("PASSWORD").toCharArray();
-		 */
+		Paragraph title = new Paragraph("RECEIPT", titleFont);
+		title.setAlignment(Element.ALIGN_CENTER);
+		document.add(title);
+		document.add(Chunk.NEWLINE);
+		PdfPTable table = new PdfPTable(2);
+		// table.setHeaderRows(1);
+
+		table.setWidthPercentage(100);
+		Image logo = Image.getInstance("tmad_logo.jpg");
+		logo.setAlignment(Element.ALIGN_CENTER);
+
+		// first movie
+		table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+
+		PdfPCell cell = new PdfPCell(logo);
+		cell.setBorder(Rectangle.NO_BORDER);
+		table.addCell(cell);
+
+		PdfPCell address = new PdfPCell();
+		address.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		Paragraph p1 = new Paragraph("TO MAKE A DIFFERENCE(TMAD),Regd.No:1506/2007,", plainFont);
+		p1.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p1);
+		Paragraph p2 = new Paragraph("(Regd. under the A.P.Societies Registration Act, 2001)", plainFont);
+		p2.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p2);
+
+		Paragraph p3 = new Paragraph("Contributions to TMAD is eligible for Tax Exemption", plainFont);
+		p3.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p3);
+
+		Paragraph p4 = new Paragraph("Under U/s 80G(2) & 5 of I.T.Act, 1961", plainFont);
+		p4.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p4);
+
+		Paragraph p5 = new Paragraph("TMAD PAN Number -AABAT6619C", plainFont);
+		p5.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p5);
+
+		Paragraph p6 = new Paragraph("2-1-7/2/88, Venkateswara Enclave, Upparpally,", plainFont);
+		p6.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p6);
+
+		Paragraph p7 = new Paragraph("Rajendra Nagar, Hyderabad - 500048", plainFont);
+		p7.setAlignment(Element.ALIGN_RIGHT);
+		address.addElement(p7);
+
+		address.setBorder(Rectangle.NO_BORDER);
+		table.addCell(address);
+		document.add(table);
+		document.add(Chunk.NEWLINE);
+		LineSeparator separator = new LineSeparator(2, 100, BaseColor.DARK_GRAY, Element.ALIGN_CENTER, 1);
+		document.add(separator);
+
+	}
+
+	private void addDonationInformation(Document document, String name, String amount, String reason, String format,
+			String date, String receiptNumber) throws DocumentException
+	{
+		String certNumber = "Receipt Number : " + receiptNumber
+				+ "                                                                " + "Date  " + date;
+		Paragraph certParagraph = new Paragraph(certNumber, plainFont);
+		certParagraph.setAlignment(Element.ALIGN_CENTER);
+		document.add(certParagraph);
+
+		document.add(Chunk.NEWLINE);
+		document.add(Chunk.NEWLINE);
+		document.add(new Paragraph("Received with thanks from " + name.toUpperCase() + "  Rupees "
+				+ amount.toUpperCase() + " towards  " + reason.toUpperCase() + "  in the form of "
+				+ format.toUpperCase() + " on   " + date));
+		document.add(Chunk.NEWLINE);
+		document.add(Chunk.NEWLINE);
+
+	}
+
+	private void addFooter(Document document) throws DocumentException
+	{
+
+		document.add(Chunk.NEWLINE);
+		LineSeparator separator = new LineSeparator(1, 100, BaseColor.DARK_GRAY, Element.ALIGN_CENTER, 1);
+		document.add(separator);
+		document.add(new Paragraph("This document is generated by OCMS on " + Calendar.getInstance().getTime(),
+				smallBold));
+		document.add(Chunk.NEWLINE);
+		document.add(new Paragraph(
+				"This receipt is generated in the demo mode and should not be treated as REAL/ORIGINAL, DEMO purpose only",
+				redFont));
+	}
+
+	public void generateReceipt(String name, String amount, String reason, String format, String date,
+			String receiptNumber)
+			throws IOException, GeneralSecurityException
+	{
+		try
+		{
+			String fileName = name;
+			Document document = new Document();
+
+			FileOutputStream out = new FileOutputStream(fileName + ".pdf");
+			PdfWriter.getInstance(document, out);
+			document.open();
+			addPageTitle(document);
+			addDonationInformation(document, name, amount, reason, format, date, receiptNumber);
+			addFooter(document);
+			document.close();
+			sign(fileName + ".pdf", fileName + "_signed.pdf");
+		}
+		catch (DocumentException de)
+		{
+			throw new IOException(de.getMessage());
+		}
+	}
+
+	private void sign(String source, String destination) throws IOException, GeneralSecurityException,
+			DocumentException
+	{
 		String path = "/Users/hmallepally/dsc/HARINATH MALLEPALLY.pfx";
-		char[] pass = "password".toCharArray();
+		char[] pass = "1234".toCharArray();
 
 		BouncyCastleProvider provider = new BouncyCastleProvider();
 		Security.addProvider(provider);
@@ -76,11 +200,27 @@ public class PDFGenerator
 		Certificate[] chain = ks.getCertificateChain(alias);
 		PDFGenerator app = new PDFGenerator();
 		app.generateSimplePDF();
-		app.sign(SRC, DEST, chain, pk, DigestAlgorithms.SHA256, provider.getName(), CryptoStandard.CMS, "OCMS",
-				"HYDERABAD", null, null, null, 0);
+		app.sign(source, destination, chain, pk, DigestAlgorithms.SHA256, provider.getName(), CryptoStandard.CMS,
+				"OCMS", "HYDERABAD", null, null, null, 0);
 	}
 
-	public void sign(String src, String dest, Certificate[] chain, PrivateKey pk, String digestAlgorithm,
+	public static final String SRC = "hello.pdf";
+	public static final String DEST = "hello_cacert.pdf";
+
+	public static void main(String[] args) throws IOException, GeneralSecurityException, DocumentException
+	{
+		/**
+		 * Properties properties = new Properties(); properties.load(new
+		 * FileInputStream("c:/home/blowagie/key.properties")); String path = properties.getProperty("PRIVATE"); char[]
+		 * pass = properties.getProperty("PASSWORD").toCharArray();
+		 */
+
+		PDFGenerator generator = new PDFGenerator();
+		generator.generateReceipt("Harinath", "Two thousand", " Demo purpose ", " Online", "09/07/2013", "202");
+
+	}
+
+	private void sign(String src, String dest, Certificate[] chain, PrivateKey pk, String digestAlgorithm,
 			String provider, CryptoStandard subfilter, String reason, String location, Collection<CrlClient> crlList,
 			OcspClient ocspClient, TSAClient tsaClient, int estimatedSize) throws GeneralSecurityException,
 			IOException, DocumentException
@@ -92,7 +232,7 @@ public class PDFGenerator
 		PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
 		appearance.setReason(reason);
 		appearance.setLocation(location);
-		appearance.setVisibleSignature(new Rectangle(36, 748, 144, 780), 1, "sig");
+		appearance.setVisibleSignature(new Rectangle(36, 748, 600, 780), 1, "OCMS");
 		// Creating the signature
 		ExternalSignature pks = new PrivateKeySignature(pk, digestAlgorithm, provider);
 		ExternalDigest digest = new BouncyCastleDigest();
@@ -100,7 +240,7 @@ public class PDFGenerator
 				subfilter);
 	}
 
-	public void generateSimplePDF() throws IOException
+	private void generateSimplePDF() throws IOException
 	{
 
 		try
